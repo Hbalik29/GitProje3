3b01158fb5e3ebe512246f97801920304ee5cbe3
Hbalik29 <hbalik29@outlook.com>
2023-01-26 19:29:52 +0200

    task 2 yapıldı

--- src/gun01/Student.java
+++ src/gun01/Student.java
@@ -3,7 +3,9 @@ package gun01;
 public class Student {
     public static void main(String[] args) {
 
-        System.out.println("merhaba1");
-        
+        System.out.println("merhaba 1");
+
+        System.out.println("task 2 yapildi");
+
     }
 }
